8cb98a989962696a9f0a585c1757f53fbd288ed6
Aakash-mistry <115260069+DLifestyle@users.noreply.github.com>
2022-10-08 12:07:33 +0530

    first uploads

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const colors = require("tailwindcss/colors");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+     content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+     theme: {
+          extend: {
+               colors: {
+                    primary: colors.emerald,
+                    secondary: colors.blue,
+               },
+               fontFamily: {
+                    sans: ["'Ubuntu', sans-serif"],
+               },
+          },
+     },
+     plugins: [],
+};
